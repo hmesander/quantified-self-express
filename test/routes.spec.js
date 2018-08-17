29ad9348f4b40850014178db43120b26af90b42b
hmesander <haleymesander@gmail.com>
2018-08-17 08:56:09 -0600

    Adds test for retrieving all foods

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -16,8 +16,53 @@ describe('Client Routes', () => {
       done();
     });
   });
+
+  it('should return a 404 for a route that does not exist', done => {
+    chai.request(server)
+    .get('/route/does/not/exist')
+    .end((err, response) => {
+      response.should.have.status(404);
+      done();
+    });
+  });
 });
 
 describe('API Routes', () => {
+  before((done) => {
+    database.migrate.latest()
+    .then(() => done())
+    .catch(error => {
+      throw error;
+    });
+  });
 
+  beforeEach((done) => {
+    database.seed.run()
+    .then(() => done())
+    .catch(error => {
+      throw error;
+    });
+  });
+
+  describe('GET /api/v1/foods', () => {
+    it('should return all of the papers', done => {
+      chai.request(server)
+      .get('/api/v1/foods')
+      .end((err, response) => {
+        response.should.have.status(200);
+        response.should.be.json;
+        response.body.should.be.a('array');
+        response.body.length.should.equal(115);
+        response.body[0].should.have.property('id');
+        response.body[0].id.should.equal(1);
+        response.body[0].should.have.property('name');
+        response.body[0].name.should.equal('');
+        response.body[0].should.have.property('calories');
+        response.body[0].calories.should.equal('');
+        response.body[0].should.not.have.property('created_at');
+        response.body[0].should.not.have.property('updated_at');
+        done();
+      });
+    });
+  });
 });
