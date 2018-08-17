f14b35f80af7dd2e4dad7c785999f7a33d952bc3
hmesander <haleymesander@gmail.com>
2018-08-17 08:44:01 -0600

    Adds test for root path html/text

---
+++ test/routes.spec.js
@@ -0,0 +1,23 @@
+const chai = require('chai');
+const should = chai.should();
+const chaiHttp = require('chai-http');
+const server = require('../server');
+
+chai.use(chaiHttp);
+
+describe('Client Routes', () => {
+  it('should return the root page with text', done => {
+    chai.request(server)
+    .get('/')
+    .end((err, response) => {
+      response.should.have.status(200);
+      response.should.be.html;
+      response.res.text.should.equal('Welcome to the Quantified Self - Express API!');
+      done();
+    });
+  });
+});
+
+describe('API Routes', () => {
+
+});
