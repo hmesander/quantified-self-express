efce6c813eec6edef91f0dfa8cc0b29d552397f9
hmesander <haleymesander@gmail.com>
2018-08-18 12:35:46 -0600

    Implements retrieving all meals - happy path

--- server.js
+++ server.js
@@ -95,6 +95,16 @@ app.patch('/api/v1/foods/:id', (request, response) => {
   });
 });
 
+app.get('/api/v1/meals', (request, response) => {
+  database('meals').select('id', 'name')
+  .then((meals) => {
+    response.status(200).json(meals)
+  })
+  .catch(error => {
+    response.status(500).json({ error });
+  });
+});
+
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
