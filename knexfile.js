81c3395fc3247d509373268120aefcd35e6bc628
hmesander <haleymesander@gmail.com>
2018-08-21 01:50:06 -0600

    WIP: Database connections

--- knexfile.js
+++ knexfile.js
@@ -28,9 +28,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-    connection: process.env.DATABASE_URL + `?ssl=true`,
-    },
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: './db/migrations'
     },
